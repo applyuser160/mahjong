f0bde2869c829b79cd21fe5f6483544abc9195c5
applyuser160 <syunsuke130121@gamil.com>
2024-11-20 22:02:40 +0900

    add method of hand

--- src/hand.rs
+++ src/hand.rs
@@ -11,4 +11,19 @@ impl Hand {
     pub fn new() -> Self {
         Self { tiles: Vec::new() }
     }
+
+    #[allow(dead_code)]
+    pub fn push_tile(&mut self, tile: Tile) {
+        self.tiles.push(tile);
+    }
+
+    #[allow(dead_code)]
+    pub fn pop_tile(&mut self, index: usize) {
+        self.tiles.remove(index);
+    }
+
+    #[allow(dead_code)]
+    pub fn sort(&mut self) {
+        self.tiles.sort_by(|a, b| a.name.cmp(&b.name));
+    }
 }
